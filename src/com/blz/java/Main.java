85530a1f37457b609befad8240681cd208a545b1
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-27 20:01:58 +0530

    Added : Given Three Integers Find The Maximum

--- src/com/blz/java/Main.java
+++ src/com/blz/java/Main.java
@@ -3,7 +3,8 @@ package com.blz.java;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to check max of three variables");
-	}
+	     System.out.println("Welcome to check max of three variables");
+	        Integer x = 10,y=30,z=25;
+	        TestMaximum.testMaximum(x,y,z);	}
 
 }
