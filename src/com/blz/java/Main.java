3be8751bd85f60b7fff16e985fe79b776bd4b064
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-27 20:08:34 +0530

    Added : Given Three Integers Find the Maximum

--- src/com/blz/java/Main.java
+++ src/com/blz/java/Main.java
@@ -2,9 +2,14 @@ package com.blz.java;
 
 public class Main {
 
-	public static void main(String[] args) {
-	     System.out.println("Welcome to check max of three variables");
-	        Integer x = 10,y=30,z=25;
-	        TestMaximum.testMaximum(x,y,z);	}
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to check max of three variables");
+	        String x = "jack", y = "king", z = "queen";
+	        Integer a = 25, b = 24, c = 19;
+	        Float p = 1.23f, q = 2.2f, r = 0.2f;
+	        TestMaximum.testMaximum(x, y, z);
+	        TestMaximum.testMaximum(a, b, c);
+	        TestMaximum.testMaximum(p, q, r);
+	 }
 
 }
--- src/com/blz/java/TestMaximum.java
+++ src/com/blz/java/TestMaximum.java
@@ -1,26 +1,26 @@
 package com.blz.java;
 
-public class TestMaximum {
-	    int x,y,z;
-	//constructor
-	    public TestMaximum(int x, int y, int z) {
-	        this.x = x;
-	        this.y = y;
-	        this.z = z;
-	    }
-	//checking max of three integers
-	    public static Integer testMaximum(Integer x,Integer y,Integer z){
-	        Integer max=x;//Initializing x as greater
-	        if(y.compareTo(max)>0)
-	            max=y;//as of now y is greater
+public class TestMaximum <T extends Comparable<T>>{
+    T x,y,z;
+//constructor
+    public TestMaximum(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+//checking max of three integers
+    public static<T extends Comparable<T>> T testMaximum(T x,T y,T z){
+        T max=x;//Initializing x as greater
+        if(y.compareTo(max)>0)
+            max=y;//as of now y is greater
 
-	        if(z.compareTo(max)>0)
-	            max=z;//now z is greater
-	        printMax(x,y,z,max);
-	        return max;
+        if(z.compareTo(max)>0)
+            max=z;//now z is greater
+        printMax(x,y,z,max);
+        return max;
 
-	    }
-	    public static void printMax(Integer x,Integer y,Integer z,Integer max){
-	        System.out.printf("the max of %s , %s and %s is %s\n",x,y,z,max);
-	    }
+    }
+    public static <T>void printMax(T x,T y,T z,T max){
+        System.out.printf("the max of %s , %s and %s is %s\n",x,y,z,max);
+    }
 }
